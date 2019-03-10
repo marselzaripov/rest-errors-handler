6eea974577c45480d4b12d528e7ea0ca1cfe7453
Марсель <marsel-zaripov-1988@mail.ru>
2019-03-10 09:56:24 +0300

    тест

--- frontend/views/layouts/main.php
+++ frontend/views/layouts/main.php
@@ -22,6 +22,7 @@ AppAsset::register($this);
     <title><?= Html::encode($this->title) ?></title>
     <?php $this->head() ?>
 </head>
+
 <body>
 <?php $this->beginBody() ?>
 
@@ -60,7 +61,7 @@ AppAsset::register($this);
     ]);
     NavBar::end();
     ?>
-
+    
     <div class="container">
         <?= Breadcrumbs::widget([
             'links' => isset($this->params['breadcrumbs']) ? $this->params['breadcrumbs'] : [],
